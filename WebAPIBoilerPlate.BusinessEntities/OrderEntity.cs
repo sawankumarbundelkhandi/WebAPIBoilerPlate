4ec835d4c2e5f3df4a17a508ffd2e452a0e084fb
CESCHN\sawankumarb <sawan.bundelkhandi@berry.net>
2017-07-04 20:27:28 +0530

    business entity

---
+++ WebAPIBoilerPlate.BusinessEntities/OrderEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPIBoilerPlate.BusinessEntities
+{
+    public class OrderEntity
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public int? ShipVia { get; set; }
+        public decimal? Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+
+        public ICollection<OrderDetailEntity> OrderDetails { get; set; }
+    }
+}
